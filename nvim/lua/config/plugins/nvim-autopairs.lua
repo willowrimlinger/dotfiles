538cfe6c8fa5292c6d3a07e3378bf0c26cc6dcc7
Willow Rimlinger <git@willowr.dev>
2026-02-05 14:11:10 -0500

    Update autopairs to work well with lisp quote

--- nvim/lua/config/plugins/nvim-autopairs.lua
+++ nvim/lua/config/plugins/nvim-autopairs.lua
@@ -1,10 +1,11 @@
 return {
     'windwp/nvim-autopairs',
     event = "InsertEnter",
-    config = true,
-    opts = {
-        fast_wrap = {}
-    }
-    -- use opts = {} for passing setup options
-    -- this is equivalent to setup({}) function
+    config = function()
+        autopairs = require("nvim-autopairs");
+        autopairs.setup({
+            fast_wrap = {}
+        });
+        autopairs.get_rules("'")[1].not_filetypes = { "scheme", "lisp", "racket" };
+    end
 }
